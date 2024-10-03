8a1719ca9bf3a4ab1613d9571882ac5696d25a7a
ClementCastex <150129015+ClementCastex@users.noreply.github.com>
2024-10-03 11:07:43 +0200

    UserChangeName  pour changer le repo facilement et en provider

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React from 'react';
-import './App.css';
-import Button from './components/atoms/Buttons/Button'; 
-import { CardRepoGitHub, CouleurBG, Avatar } from './components/atoms/Containers';
-import { Heading, Paragraph, Hyperlien } from './components/atoms/Typo'; 
-import ListRepoGitHub from './components/molecules/ListRepoGitHub';
+import React from "react";
+import "./App.css";
+import Button from "./components/atoms/Buttons/Button";
+import {
+  CardRepoGitHub,
+  CouleurBG,
+  Avatar,
+} from "./components/atoms/Containers";
+import { Heading, Paragraph, Hyperlien } from "./components/atoms/Typo";
+import ListRepoGitHub from "./components/molecules/ListRepoGitHub";
+import UsernameChanger from "./components/Settings/UsernameChanger";
+import { GitHubProvider, useGitHub } from "../../context/GithubContext";
 
 const App = () => {
   const handleClick = () => {
-    alert('Button clicked!');
+    alert("Button clicked!");
   };
 
   return (
     <CouleurBG color="#1e2124">
       <Heading color="#FFF">Mon Portfolio</Heading>
-      <Paragraph>
-  Voici un exemple de paragraphe dans mon portfolio. 
-</Paragraph>
+      <Paragraph>Voici un exemple de paragraphe dans mon portfolio.</Paragraph>
 
-      <Hyperlien href='#'color='#7289da'>Pls Help Me</Hyperlien>
-      <Button label="Voir mes projets" onClick={handleClick} className="primary" />
+      <Hyperlien href="#" color="#7289da">
+        Pls Help Me
+      </Hyperlien>
+      <Button
+        label="Voir mes projets"
+        onClick={handleClick}
+        className="primary"
+      />
+      <UsernameChanger />
       <ListRepoGitHub />
     </CouleurBG>
   );
 };
-
 
 export default App;
--- src/components/molecules/ListRepoGitHub.jsx
+++ src/components/molecules/ListRepoGitHub.jsx
@@ -1,25 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import {CardRepoGitHub,CouleurBG} from '../atoms/Containers/';
-import { Heading, Hyperlien, Paragraph } from '../atoms/Typo';
-
+import React, { useState, useEffect } from "react";
+import { CardRepoGitHub, CouleurBG } from "../atoms/Containers/";
+import { Heading, Hyperlien, Paragraph } from "../atoms/Typo";
+import { GitHubProvider, useGitHub } from "../../context/GithubContext";
 
 const ListRepoGitHub = () => {
   const [projects, setProjects] = useState([]);
+  const { username } = useGitHub();
 
   useEffect(() => {
-    const username = 'LewisEMO'; // Remplace par ton pseudo GitHub
-
     fetch(`https://api.github.com/users/${username}/repos`)
-      .then(response => response.json())
-      .then(data => setProjects(data))
-      .catch(error => console.error('Erreur:', error));
-  }, []);
+      .then((response) => response.json())
+      .then((data) => setProjects(data))
+      .catch((error) => console.error("Erreur:", error));
+  }, [username]);
 
   return (
     <div>
-      <Heading level={1}color="#fff">Mes Projets GitHub</Heading>
+      <Heading level={1} color="#fff">
+        Mes Projets GitHub
+      </Heading>
       <CouleurBG color="#1e2124" id="github-projects">
-        {projects.map(repo => (
+        {projects.map((repo) => (
           <CardRepoGitHub
             key={repo.id}
             name={repo.name}
