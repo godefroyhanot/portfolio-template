18fa9924c6344c4ad628c967e01837b295ec3190
godefroyhanot <godefroy.hanot@hotmail.fr>
2024-10-03 09:48:08 +0200

    Menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home, Projets, Contact } from './components/pages';
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projets" element={<Projets />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
-  );
-};
 
+    const [slug, setSlug] = useState("home");
+    let tableauMenu = [
+      {
+        slug: "home",
+        title: "Accueil",
+        component: <Home />,
+      },
+      {
+        slug: "projets",
+        title: "Projets",
+        component: <Projets />,
+      },
+      {
+        slug: "contact",
+        title: "Contact",
+        component: <Contact />,
+      },
+    ];
+
+    // Déplace cette fonction à l'intérieur du composant App
+    const displayPage = () => {
+      switch (slug) {
+        case "home":
+          return <Home />;
+        case "projets":
+          return <Projets />;
+        case "contact":
+          return <Contact />;
+        default:
+          return <Home />;
+      }
+    };
+
+    return (
+      <div className="App">
+        {/* Appelle displayPage pour rendre la page */}
+        {displayPage()}
+      </div>
+    );
+}
 
 export default App;
