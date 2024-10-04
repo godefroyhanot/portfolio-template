b917201f596885a8f8db74f805e62802aafc29eb
ClementCastex <150129015+ClementCastex@users.noreply.github.com>
2024-10-04 17:23:08 +0200

    dernier probleme résolu

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,5 +1,5 @@
-
 import Menu from "./Menu";
 import AddProject from "./AddProject";
 import ProjectList from "./ProjectList";
+import ListRepoGitHub from "./ListRepoGitHub";
 export { ListRepoGitHub, Menu, ProjectList, AddProject };
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,23 +1,12 @@
 import React from "react";
 import { Heading, Paragraph, Hyperlien } from "../atoms/Typo";
 import { BgColor } from "../atoms/Containers";
-import { Carousel } from "../molecules/Widgets";
 
 const Home = () => {
   return (
     <BgColor>
       <Heading>Mon Portfolio</Heading>
-      <Carousel
-        handleLeftCarouselElt={(<>[Precedent]</>)}
-        handleRightCarouselElt={(<>[Suivant]</>)}
-        handleCarouselElt={<>[.]</>}
-        content={[
-          (<div>Hello</div>),
-          (<div>yo</div>),
-          (<div>Salut</div>)
-        ]}
-      />
-    </BgColor >
+    </BgColor>
   );
 };
 
